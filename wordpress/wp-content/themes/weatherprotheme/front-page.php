8e0a7d017f301dcac6e012531e66f57c01088236
Aatu Anttila <L5275@student.jamk.fi>
2020-06-15 21:16:01 +0300

    Advanced custom fields plugin added

--- wordpress/wp-content/themes/weatherprotheme/front-page.php
+++ wordpress/wp-content/themes/weatherprotheme/front-page.php
@@ -33,14 +33,28 @@
                 $response = file_get_contents($url);
                 $response = json_decode($response, true);
 
-                $weather_now = $response["currently"]["temperature"];
-                $weather_tmrw =  $response["daily"]["data"][1]["temperatureHigh"];
-                $weather_day_after_tmrw = $response["daily"]["data"][2]["temperatureHigh"];
+                $weather_now = round($response["currently"]["temperature"], 0);
+                $weather_now_summary = $response["currently"]["summary"];
+
+                $weather_tmrw =  round($response["daily"]["data"][1]["temperatureHigh"], 0);
+                $weather_tmrw_summary = $response["daily"]["data"][1]["summary"];
+
+                $weather_day_after_tmrw = round($response["daily"]["data"][2]["temperatureHigh"], 0);
+                $weather_day_after_tmrw_summary = $response["daily"]["data"][2]["summary"];
+
+                // +-sign if temperature positive
+                $weather_now = sprintf("%+d",$weather_now);
+                $weather_tmrw = sprintf("%+d",$weather_tmrw);
+                $weather_day_after_tmrw = sprintf("%+d",$weather_day_after_tmrw);
 
                 echo "<div>";
                     echo "<h3>" . get_the_title() . "</h3>";
                     echo "<div class='weather_container'>";
-                        echo "<p>Nyt: " . $weather_now  . "</p></p>Huomenna: " . $weather_tmrw . "</p><p>Ylih.: " . $weather_day_after_tmrw . "</p>";
+                        echo "
+                            <p>" . date("l") . "<br>" . $weather_now_summary . "<br>" . $weather_now  . "</p>
+                            <p>" . date("l", strtotime("+1day")) . " <br>$weather_tmrw_summary<br>" . $weather_tmrw . "</p>
+                            <p>" . date("l", strtotime("+2day")) . " <br>$weather_day_after_tmrw_summary<br>" . $weather_day_after_tmrw . "</p>"
+                        ;
                     echo "</div>";
                 echo "</div>";
                 
@@ -49,9 +63,22 @@
             echo "<p>Käy lisäämässä uusi 'Sijainnit'-custom post type.</p>";
         }
         ?>
-
     </div>
 </header>
+
+<section class="info">
+    <?php
+
+        $field = get_field('title');
+        echo $field;
+
+    ?>
+    <h4>Hello world</h4>
+    <h4><?php the_field('catchphrase'); ?></h4>
+    <h3><?php the_field('subtitle'); ?></h3>
+
+</section>
+
 <?php if ( ! dynamic_sidebar( 'footer-area' ) ) : ?>
   <section class="footer">
       <h2>Contact us</h2>
